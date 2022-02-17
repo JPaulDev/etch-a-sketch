bc8cd687d4fab6f80286e5c9e593ea80207592e7
JonPaulCode <jwatson88@live.co.uk>
2022-02-17 20:44:44 +0000

    Added in a slider that can be used to generate different numbers of pixels/blocks inside the canvas.

--- scripts/game-script.js
+++ scripts/game-script.js
@@ -1,15 +1,21 @@
 const div = document.querySelector('.container');
+const slider = document.querySelector('input');
+const sliderValue = document.querySelector('.slider-value');
 
-function generatePixels() {
-  for (let i = 0; i < 900; i++) {
+function generatePixels(number) {
+  for (let i = 0; i < number * number; i++) {
     const pixelBlock = document.createElement('div');
     pixelBlock.style.float = 'left';
     pixelBlock.style.borderTop = '1px solid black';
     pixelBlock.style.borderLeft = '1px solid black';
-    pixelBlock.style.height = '20px';
-    pixelBlock.style.width = '20px';
+    pixelBlock.style.height = '40px';
+    pixelBlock.style.width = '40px';
     div.appendChild(pixelBlock);
   }
 }
 
-generatePixels();
+slider.addEventListener('input', function updateValue() {
+  div.replaceChildren();
+  sliderValue.textContent = this.value;
+  generatePixels(slider.value);
+});
