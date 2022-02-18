e828d488f60f51c776fbb9a52ce59f3df34a6674
JonPaulCode <jwatson88@live.co.uk>
2022-02-18 11:55:02 +0000

    Improved logic of generatePixels function, now draws varying numbers of blocks that grow or shrink to fill the canvas correctly.

--- scripts/game-script.js
+++ scripts/game-script.js
@@ -1,21 +1,27 @@
-const div = document.querySelector('.container');
+const div = document.querySelector('.canvas');
 const slider = document.querySelector('input');
 const sliderValue = document.querySelector('.slider-value');
 
-function generatePixels(number) {
-  for (let i = 0; i < number * number; i++) {
+function generatePixels(n) {
+  for (let i = 0; i < n * n; i++) {
     const pixelBlock = document.createElement('div');
+    pixelBlock.style.height = `${600 / n}px`;
+    pixelBlock.style.width = `${600 / n}px`;
     pixelBlock.style.float = 'left';
     pixelBlock.style.borderTop = '1px solid black';
     pixelBlock.style.borderLeft = '1px solid black';
-    pixelBlock.style.height = '40px';
-    pixelBlock.style.width = '40px';
     div.appendChild(pixelBlock);
   }
 }
 
+// Called on load to draw initial blocks
+generatePixels(30);
+
 slider.addEventListener('input', function updateValue() {
-  div.replaceChildren();
   sliderValue.textContent = this.value;
+});
+
+slider.addEventListener('mouseup', () => {
+  div.replaceChildren();
   generatePixels(slider.value);
 });
