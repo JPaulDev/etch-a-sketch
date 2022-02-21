78fba81ea539fec5f3312459c5df5280a0bfb19b
JonPaulCode <jwatson88@live.co.uk>
2022-02-21 14:09:22 +0000

    Added in eraser functionality along with ability to select/deselect tools. All mentions of brush renamed to pen. Removed the ability to select/highlight any text which would sometimes happen accidentally while drawing.

--- scripts/game-script.js
+++ scripts/game-script.js
@@ -2,18 +2,32 @@ const canvas = document.querySelector('.canvas');
 const slider = document.querySelector('.slider');
 const sliderValue = document.querySelector('.slider-value');
 const colourSelector = document.querySelector('.colour-selector');
+const tools = document.querySelectorAll('.tools');
 
+let currentTool = 'pen';
 let mouseDown = false;
+
 window.addEventListener('pointerdown', () => {
   mouseDown = true;
 });
-
 window.addEventListener('pointerup', () => {
   mouseDown = false;
 });
 
-function brush() {
-  if (mouseDown) this.style.background = colourSelector.value;
+tools.forEach((tool) => {
+  tool.addEventListener('click', function changeTool() {
+    document.querySelector('.selected').classList.remove('selected');
+    currentTool = this.getAttribute('id');
+    this.classList.add('selected');
+  });
+});
+
+function toolAction() {
+  if (mouseDown && currentTool === 'pen') {
+    this.style.background = colourSelector.value;
+  } else if (mouseDown && currentTool === 'eraser') {
+    this.style.background = '#FFFFFF';
+  }
 }
 
 function generatePixels(n) {
@@ -21,7 +35,7 @@ function generatePixels(n) {
     const pixelBlock = document.createElement('div');
     pixelBlock.style.height = `${(600 / n / 600) * 100}%`;
     pixelBlock.style.width = `${(600 / n / 600) * 100}%`;
-    pixelBlock.addEventListener('mouseover', brush);
+    pixelBlock.addEventListener('mouseover', toolAction);
     canvas.appendChild(pixelBlock);
   }
 }
